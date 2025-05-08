0b6a623b242f557e33c694e83ec58d4bc57939ce
Geovannio Vinhas <gvinhas@tic.gov.tl>
2025-05-08 11:28:19 +0900

    Remove https in api url

--- src/environments/environment.testing.ts
+++ src/environments/environment.testing.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    url: 'https://api-cristest.bancocentral.tl',
-    apiUrl: 'https://api-cristest.bancocentral.tl/api/v1',
+    url: 'http://api-cristest.bancocentral.tl',
+    apiUrl: 'http://api-cristest.bancocentral.tl/api/v1',
 };
